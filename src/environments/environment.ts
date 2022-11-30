092419a35cb1097618ccab4fe7d9fec86d5d9f5f
Daniel Soares <daniel.soares@intapp.com>
2022-11-29 23:30:47 -0300

    Customizations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ export const environment = {
   // replace the data below with your personal data
   personal: {
     caricature: "gbastos-illustration@2x-mask.png",
-    name: "Guilherme Borges Bastos",
-    birth: "1993-06-29",
-    email: "guilhermeborgesbastos@gmail.com",
-    phone: "+31 68 733 3020",
-    location: "Rotterdam, South Holland, Netherlands"
+    name: "Daniel Godinho Soares",
+    birth: "1998-06-02",
+    email: "dangodinho98@gmail.com",
+    phone: "+55 31 9 92521539",
+    location: "Minas Gerais, Brazil"
   },
   // replace the dummy data below with the real firebase configs
   firebaseConfig: {
